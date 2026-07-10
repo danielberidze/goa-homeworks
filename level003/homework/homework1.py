8ce9fd8e9a090b9d09cd639e7e18392c631dfca2
danielberidze <daniel.beridze.mail@gmail.com>
2026-07-10 18:03:49 +0400

    second commit

---
+++ level003/homework/homework1.py
@@ -0,0 +1,15 @@
+firstname="daniel"
+age=11
+country="rustavi"
+
+print(firstname)
+print(age)
+print(country)
+
+color="black"
+firstname="daniel"
+age=11
+
+print(color)
+print(firstname)
+print(age)
