c83dcba46e825e31dde94cd6d26e0f641e1259f7
danielberidze <daniel.beridze.mail@gmail.com>
2026-07-11 12:32:35 +0400

    second comit

---
+++ level004/klaswork/klaswork1.py
@@ -0,0 +1,21 @@
+#input ბრძანება გამოიყენება კომპიუტერში ინფორმაციის შესატანად მაგ. მიკროფონი, მაუსი და კამერა.
+#string(ბრჭყალებიანი ციფრები ან ჭინადადებები). string უბრალო წინადადება არის რომელიც print ფუნქციას გამოაქ ტერმინალში
+#float არის ათწილადი რიცხვი მაგ.2.5
+#int უბრალო რიცხვია მაგ. 1,2 და 3
+
+firstname=input("pleas enter your firstname:")
+lastname=input("pleas enter your lastname:")
+city=input("pleas enter your city's name")
+color=input("pleas enter your favorite color:")
+
+print(firstname+lastname+city+color)
+
+#input ს შეაქ კომპიუტერში ინფორმაცია მაგ. მაუსის,მიკროფონის,კლავიატურის,კამერი და ფლეშკის(როდესაც კომპიუტერზე წერ ინფორმაციას)
+#output ს გააქ კომპიუტერიდან ინფორმაცია როგორიცაა: ეკრანის გამოსახულება,დინამიკის ხმაური,პრინტერიდან ამობეჭდვა,
+#ფლეშკა (როდესაც კომპიუტერიდან წერ ინფორმაციას ფლეშკაზე) და კარტა (როდესაც კარდრიდერით გამოგაქ ინფორმაცია კომპიუტერიდან)
+
+username=input("pleas enter your name")
+userage=input("pleas enter your age")
+
+print(username)
+print(userage)
---
+++ my foldet/01.py
@@ -0,0 +1,3 @@
+firstname=input("enter your firstname:")
+lastname=input("enter your lastname")
+print("I love you"+firstname+lastname)
